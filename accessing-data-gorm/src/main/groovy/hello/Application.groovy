93521e230e75fe4efe6f3a61dc7a43f1f2bfc955
Sven Ehrke <sven.ehrke@canoo.com>
2014-12-30 13:28:26 +0100

    upgrade to spring boot 1.2.0

--- accessing-data-gorm/src/main/groovy/hello/Application.groovy
+++ accessing-data-gorm/src/main/groovy/hello/Application.groovy
@@ -2,9 +2,10 @@ package hello
 
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration
+import org.springframework.boot.autoconfigure.groovy.template.GroovyTemplateAutoConfiguration
 import org.springframework.context.annotation.ComponentScan
 
-@EnableAutoConfiguration
+@EnableAutoConfiguration(exclude = GroovyTemplateAutoConfiguration) // see https://github.com/spring-projects/spring-boot/issues/2190
 @ComponentScan
 class Application {
     static void main(String[] args) {
